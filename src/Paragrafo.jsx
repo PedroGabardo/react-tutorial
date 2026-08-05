2becd16ff3aa2e66615a549c9cb3eb03a01337a1
Pedro <pedrotestagabardo@gmail.com>
2026-08-05 11:00:07 -0300

    first commit

---
+++ src/Paragrafo.jsx
@@ -0,0 +1,12 @@
+import saddam from './assets/saddam.jpg'
+
+function Paragrafo() {
+    return (
+    <>
+    <p>é, é um parágrafo mesmo</p>
+    <p><img src={saddam} width={350} height={350}/></p>
+    </>
+    )
+}
+
+export default Paragrafo
