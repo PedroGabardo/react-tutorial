d639e2d426606b86531bfcc5c6850333c9ab151b
Pedro <pedrotestagabardo@gmail.com>
2026-08-14 08:41:26 -0300

    criação da lista de tarefas

---
+++ src/TarefasLista.jsx
@@ -0,0 +1,16 @@
+import TarefasItem from "./TarefasItem";
+
+const TarefasLista = ({ tarefas, onEditTarefa, onDeleteTarefa }) => {
+    return (
+        <ul>
+            {tarefas.map((tarefa) => (
+                <TarefasItem key={tarefa.id}
+                    tarefa={tarefa}
+                    onEditTarefa={onEditTarefa}
+                    onDeleteTarefa={onDeleteTarefa} />
+            ))}
+        </ul>
+    );
+}
+
+export default TarefasLista;
